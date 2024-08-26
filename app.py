0e615a3a95fb1f91ec930b8c341b966c7e9d0648
Flempopo <admin@aoe2central.com>
2024-08-26 13:35:10 +0200

    Implemented search and player details features

--- app.py
+++ app.py
@@ -5,19 +5,25 @@ app = Flask(__name__)
 
 def fetch_players(start=1, count=100):
     response = requests.get(f"https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=3&start={start}&count={count}")
-    return response.json()
+    players = response.json()['leaderboard']
+    
+    # Round the win rate to two decimal places for each player
+    for player in players:
+        player['win_rate'] = round((player['wins'] / player['games']) * 100, 2)
+    
+    return players
 
 @app.route('/')
 def leaderboard():
     data = fetch_players()
-    return render_template('index.html', data=data['leaderboard'])
+    return render_template('index.html', data=data)
 
 @app.route('/load-more')
 def load_more():
     start = int(request.args.get('start', 101))
     count = int(request.args.get('count', 100))
     data = fetch_players(start=start, count=count)
-    return jsonify(data['leaderboard'])
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
